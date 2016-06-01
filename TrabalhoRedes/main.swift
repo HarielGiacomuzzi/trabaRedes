0fe81534c3fa085dadea86377e19412d627a20bc
Hariel Giacomuzzi <hariel.dias@acad.pucrs.br>
2016-06-01 17:46:31 -0300

    Initial Commit

---
+++ TrabalhoRedes/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  TrabalhoRedes
+//
+//  Created by Hariel Giacomuzzi on 6/1/16.
+//  Copyright © 2016 Giacomuzzi. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
